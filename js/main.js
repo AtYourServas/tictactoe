b7b56c5f97e6bec05f7ff1532cbe7cf393d5c56f
AtYourServas <joeservas@gmail.com>
2022-04-29 17:12:42 -0400

    updates to grid functionality

--- js/main.js
+++ js/main.js
@@ -1,27 +1,47 @@
 const grid = []
-//assign all sections in the bingo grid to an element in the grid array
+//assign all sections in the board grid to an element in the grid array
 for (let i = 0; i <= 8; i++) {
     grid[i] = document.getElementById('grid' + i)
 }
 
 let player1Turn = true
 
-grid[0].addEventListener('click', takeTurn)
+document.querySelector('#gridBoard').addEventListener('click', (event) => {
+    console.log(event.target.id)
+    
+    takeTurn(event.target.id)
+})
 
 
-function takeTurn() {
-  if (player1Turn == true) {
-        grid[0].classList.add('player1')
-        grid[0].classList.remove('player2')
-        player1Turn = false
-        
+function takeTurn(target) {
+
+    target = document.getElementById(target)
+
+    if (target.classList.contains('player1') || target.classList.contains('player2')) {
+        alert('This spot has been chosen.  Try another')
     }
-     else {
-        grid[0].classList.add('player2')
-        grid[0].classList.remove('player1')
-        player1Turn = true
+    else {
+        if (player1Turn == true) {
+            target.classList.add('player1')
+            target.classList.remove('player2')
+            player1Turn = false
+            
+        }
+         else {
+            target.classList.add('player2')
+            target.classList.remove('player1')
+            player1Turn = true
+        }
+        checkForWin()
     }
 }
+
+function checkForWin() {
+    if (grid[0].classList.contains('player1') && grid[1].classList.contains('player1') && grid[2].classList.contains('player1')) {
+        alert('Player 1 Wins')
+    }
+}
+
 
 //function to reset grid
 function reloadPage(){
